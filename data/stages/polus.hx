d08630d9c7d2410a1518c914e267b3bea188f269
APurples <tylersanders0714@gmail.com>
2023-09-21 20:29:16 -0500

    why does this keep happening

--- data/stages/polus.hx
+++ data/stages/polus.hx
@@ -1,24 +1,20 @@
 var speaker:FlxSprite;
 var crowd:FlxSprite;
-var bfdead:FlxSprite;
 
 function create(){
     speaker = new FlxSprite(300, 195);
     speaker.frames = Paths.getSparrowAtlas('stages/polus/speakerlonely');
     speaker.animation.addByPrefix('bop', 'speakers lonely', 24, false);
     speaker.antialiasing = true;
-    insert(members.indexOf(gf), speaker);
 
+    insert(members.indexOf(gf), speaker);
     crowd = new FlxSprite(-925, 250);
 	crowd.frames = Paths.getSparrowAtlas('stages/polus/boppers_meltdown');
 	crowd.animation.addByPrefix('bop', 'BoppersMeltdown', 24, false);
 	crowd.scrollFactor.set(1.5, 1.5);
 	crowd.antialiasing = true;
-
-    var bfdead:FlxSprite = new FlxSprite(600, 525).loadGraphic(Paths.image('stages/polus/bfdead',));
-    bfdead.setGraphicSize(Std.int(bfdead.width * .8));
-    bfdead.antialiasing = true;
-    bfdead.active = false;
+    
+    bfdead.alpha = 0;
 }
 
 function update(){
@@ -31,7 +27,7 @@ function update(){
     if (curSong == "meltdown"){
         add(speaker);
         add(crowd);
-        add(bfdead)
+        bfdead.alpha = 1;
     }
 }
 
