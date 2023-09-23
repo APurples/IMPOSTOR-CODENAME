a4b1bf882595d46230be8276c5b3f6efae96a1d3
APurples <tylersanders0714@gmail.com>
2023-09-23 12:47:40 -0500

    monochrome + other small things

---
+++ data/events/Mono Jumpscare.hx
@@ -0,0 +1,22 @@
+function create(){
+    jumpscare = new FlxSprite(0, 60).loadGraphic(Paths.image('deadpostor'));
+    jumpscare.screenCenter();
+    jumpscare.x -= 455;
+    jumpscare.y -= 85;
+    jumpscare.scale.x = jumpscare.scale.y = 1.125;
+    jumpscare.alpha = 0;
+    add(jumpscare);
+}
+
+function onEvent(e:EventGameEvent){
+    if (!FlxG.save.data.jumpscares){
+        if (e.event.name == "Mono Jumpscare"){
+            if (e.event.params[0] == true){
+                jumpscare.alpha = 1;
+            }
+            if (e.event.params[0] == false){
+                FlxTween.tween(jumpscare, {alpha: 0}, e.event.params[1]);
+            }
+        }
+    }
+}
--- songs/monochrome/scripts/gameplay.hx
+++ songs/monochrome/scripts/gameplay.hx
@@ -11,5 +11,39 @@ function stepHit(){
     switch(curStep){
         case 128:
             FlxTween.tween(camHUD, {alpha: 1}, .5);
+        
+        //codename devs please add notetypes already please :sob:
+        case 504:
+            if (!FlxG.save.data.screenShake){
+                FlxG.camera.shake(0.015, .725, null, true);
+            }
+        case 952:
+            if (!FlxG.save.data.screenShake){
+                FlxG.camera.shake(0.015, .725, null, true);
+            }
+        case 964:
+            if (!FlxG.save.data.screenShake){
+                FlxG.camera.shake(0.015, .275, null, true);
+            }
+        case 972:
+            if (!FlxG.save.data.screenShake){
+                FlxG.camera.shake(0.015, .275, null, true);
+            }
+        case 980:
+            if (!FlxG.save.data.screenShake){
+                FlxG.camera.shake(0.015, .275, null, true);
+            }
+        case 1116:
+            if (!FlxG.save.data.screenShake){
+                FlxG.camera.shake(0.015, .275, null, true);
+            }
+        case 1208:
+            if (!FlxG.save.data.screenShake){
+                FlxG.camera.shake(0.015, .725, null, true);
+            }
+        case 1536:
+            if (!FlxG.save.data.screenShake){
+                FlxG.camera.shake(0.015, 1.25, null, true);
+            }
     }
 }
