0ddadd0d9f35e2b7fbfe9b4eba5dda8380337186
APurples <tylersanders0714@gmail.com>
2023-09-26 22:05:14 -0500

    moved gameplay code to the stages themselves

--- data/stages/polus.hx
+++ data/stages/polus.hx
@@ -36,6 +36,7 @@ function update(){
         remove(speaker);
     }
     if (curSong == "sabotage"){
+        gf.alpha = 0;
         add(speaker);
     }
     if (curSong == "meltdown"){
@@ -48,7 +49,175 @@ function update(){
     }
 }
 
+function onSongStart(){
+    if (curSong == "meltdown"){
+        camGame.zoom += 0.015;
+        camHUD.zoom += 0.03;
+        if (!FlxG.save.data.flashingLights){
+            camGame.flash(0xADFF0000, .35);
+        }
+    }
+}
+
+function stepHit(){
+    if (curSong == 'sussus moogus'){
+        if (curStep > 383 && curStep < 641){
+            if (curStep % 8 == 0){
+                camGame.zoom += 0.015;
+                camHUD.zoom += 0.03;
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+            }
+        }
+        if (curStep > 1151 && curStep < 1273){
+            if (curStep % 64 == 0){
+                camGame.zoom += 0.015;
+                camHUD.zoom += 0.03;
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+            }
+        }
+        if (curStep > 1279 && curStep < 1533){
+            if (curStep % 8 == 0){
+                camGame.zoom += 0.015;
+                camHUD.zoom += 0.03;
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+            }
+        }
+    
+        switch(curStep){
+            case 376:
+                camGame.zoom += 0.015;
+                camHUD.zoom += 0.03;
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+            case 380:
+                camGame.zoom += 0.015;
+                camHUD.zoom += 0.03;
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+            case 1272:
+                camGame.zoom += 0.015;
+                camHUD.zoom += 0.03;
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+            case 1276:
+                camGame.zoom += 0.015;
+                camHUD.zoom += 0.03;
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+        }
+    }
+    else if (curSong == "sabotage"){
+        switch(curStep){
+            case 816:
+                gf.alpha = 1;
+                gf.playAnim("fadein", true);
+            case 820:
+                dad.playAnim("look", true);
+        }
+    
+        if (curStep > 63 && curStep < 511){
+            if (curStep % 32 == 0){
+                camGame.zoom += 0.015;
+                camHUD.zoom += 0.03;
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+            }
+        }
+        if (curStep > 511 && curStep < 577){
+            if (curStep % 64 == 0){
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+            }
+        }
+        if (curStep > 639 && curStep < 705){
+            if (curStep % 32 == 0){
+                camGame.zoom += 0.015;
+                camHUD.zoom += 0.03;
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+            }
+        }
+        if (curStep > 831 && curStep < 1473){
+            if (curStep % 32 == 0){
+                camGame.zoom += 0.015;
+                camHUD.zoom += 0.03;
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+            }
+        }
+    }
+    else if (curSong == "meltdown"){
+        switch (curStep){
+            case 1152:
+                FlxTween.tween(camGame, {alpha: 0}, 1.65);
+                FlxTween.tween(camHUD, {alpha: 0}, 1.65);
+        }
+    }
+}
+
 function beatHit(){
     speaker.animation.play('bop');
     crowd.animation.play('bop');
+
+    if (curSong == "meltdown"){
+        if (curBeat > 0 && curBeat < 32){
+            if (curBeat % 8 == 0){
+                camGame.zoom += 0.015;
+                camHUD.zoom += 0.03;
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+            }
+        }
+        if (curBeat > 31 && curBeat < 125){
+            if (curBeat % 4 == 0){
+                camGame.zoom += 0.015;
+                camHUD.zoom += 0.03;
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+            }
+        }
+        if (curBeat > 127 && curBeat < 185){
+            if (curBeat % 8 == 0){
+                camGame.zoom += 0.015;
+                camHUD.zoom += 0.03;
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+            }
+        }
+        if (curBeat > 191 && curBeat < 253){
+            if (curBeat % 4 == 0){
+                camGame.zoom += 0.015;
+                camHUD.zoom += 0.03;
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+            }
+        }
+        if (curBeat > 255 && curBeat < 281){
+            if (curBeat % 8 == 0){
+                camGame.zoom += 0.015;
+                camHUD.zoom += 0.03;
+                if (!FlxG.save.data.flashingLights){
+                    camGame.flash(0xADFF0000, .35);
+                }
+            }
+        }
+    }
 }
