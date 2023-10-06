fa4e3f488392092bff7133c4d035bfe6c9b35c17
APurples <tylersanders0714@gmail.com>
2023-10-05 19:46:35 -0500

    yeah man

--- data/states/FreeplayState.hx
+++ data/states/FreeplayState.hx
@@ -89,10 +89,11 @@ function postUpdate(){
             portrait.animation.play('red');
         case "sauces moogus":
             portrait.animation.play('chef');
+            FlxTween.tween(portrait, {alpha: 1}, .3);
         case "monochrome":
-            portrait.alpha = 0;
+            FlxTween.tween(portrait, {alpha: 0}, .3);
         case "double kill vtwo":
             portrait.animation.play('black');
-            portrait.alpha = 1;
+            FlxTween.tween(portrait, {alpha: 1}, .3);
     }
 }
