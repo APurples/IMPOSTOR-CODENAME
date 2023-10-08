9f5371732f8d03a4d8719d0c59273a2ffa4d2280
APurples <tylersanders0714@gmail.com>
2023-10-08 13:46:48 -0500

    did some small things to monochrome

--- songs/monochrome/scripts/gameplay.hx
+++ songs/monochrome/scripts/gameplay.hx
@@ -3,12 +3,13 @@ dad.alpha = 0;
 camHUD.alpha = 0;
 
 function onSongStart(){
-    dad.alpha = 1;
     dad.playAnim("hi");
 }
 
 function stepHit(){
     switch(curStep){
+        case 1:
+            dad.alpha = 1;
         case 128:
             FlxTween.tween(camHUD, {alpha: 1}, .5);
         
