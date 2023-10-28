284ae215970367ad737e725e9ee9b0c7e51eb349
APurples <tylersanders0714@gmail.com>
2023-10-28 09:39:42 -0500

    woops

--- data/stages/monochrome.hx
+++ data/stages/monochrome.hx
@@ -1,18 +1,20 @@
 function onCountdown(event:CountdownEvent){
-    if (event.swagCounter == 0){
-        if (PlayState.opponentMode){
-            for (i in playerStrums.members){
-                i.x -= 5000;
-            }
-            for (i in cpuStrums.members){
-                i.x += 320;
-            }
-        }else if (!PlayState.opponentMode){
-            for (i in playerStrums.members){
-                i.x -= 320;
-            }
-            for (i in cpuStrums.members){
-                i.x -= 5000;
+    if (!FlxG.save.data.middlescroll){
+        if (event.swagCounter == 0){
+            if (!PlayState.opponentMode){
+                for (i in playerStrums.members){
+                    i.x -= 320;
+                }
+                for (i in cpuStrums.members){
+                    i.x -= 5000;
+                }
+            }else{
+                for (i in playerStrums.members){
+                    i.x -= 5000;
+                }
+                for (i in cpuStrums.members){
+                    i.x += 320;
+                }
             }
         }
     }
