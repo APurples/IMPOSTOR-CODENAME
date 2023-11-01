308082fda37b0f786ae6cac9bdf53a780a410147
APurples <tylersanders0714@gmail.com>
2023-11-01 10:07:57 -0500

    health color shit is now in settings.hx

--- songs/settings.hx
+++ songs/settings.hx
@@ -1,5 +1,15 @@
 // basically the stuff for the custom options
 
+function postCreate(){
+    if (Options.colorHealthBar){
+        if (curStage == "polus") healthBar.createFilledBar(0xFFBB2D30,0xFF31B0D1);
+        if (curStage == "monochrome") healthBar.createFilledBar(0xFFFFFFFF,0xFF31B0D1);
+        if (curStage == "cargobay") healthBar.createFilledBar(0xFF6C5887,0xFF31B0D1);
+        if (curStage == "chef") healthBar.createFilledBar(0xFFC75E52,0xFF31B0D1);
+        if (curStage == "dlowingt") healthBar.createFilledBar(0xFFFDCC37,0xFF31B0D1);
+    }
+}
+
 function onPlayerHit(a){
     if (FlxG.save.data.hitsound){
         if (!a.note.isSustainNote){
