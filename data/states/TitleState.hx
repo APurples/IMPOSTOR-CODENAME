4d98243b91e1d34ad1dbc4c4e47ed84aeee5f774
APurples <tylersanders0714@gmail.com>
2023-11-04 14:51:08 -0500

    Update TitleState.hx

--- data/states/TitleState.hx
+++ data/states/TitleState.hx
@@ -1,5 +1,44 @@
-// just in case if youre loading the mod from the mod menu
+var flashTime:Bool = false;
 
 function create(){
     window.title = "Vs Impostor: V4";
+}
+
+function update(){
+    FlxG.camera.zoom = FlxMath.lerp(1, FlxG.camera.zoom, 0.95); // every time the camera gets zoomed it lerps back to the original zoom
+}
+
+function beatHit(){
+    switch (curBeat){
+        case 0:
+            flashTime = false;
+        case 8:
+            FlxG.camera.zoom += 0.045;
+            if (FlxG.save.data.flashingLights){
+                FlxG.camera.flash(0x86FF0000, .35);
+            }
+        case 14:
+            FlxG.camera.zoom += 0.045;
+            if (FlxG.save.data.flashingLights){
+                FlxG.camera.flash(0x86FF0000, .35);
+            }
+        case 15:
+            FlxG.camera.zoom += 0.045;
+            if (FlxG.save.data.flashingLights){
+                FlxG.camera.flash(0x86FF0000, .35);
+            }
+        case 16:
+            flashTime = true;
+    }
+}
+
+function measureHit(){
+    if (flashTime){
+        if (curMeasure % 4 == 0){
+            FlxG.camera.zoom += 0.045;
+            if (FlxG.save.data.flashingLights){
+                FlxG.camera.flash(0x86FF0000, .35);
+            }
+        }
+    }
 }
