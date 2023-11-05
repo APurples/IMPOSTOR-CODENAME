c69916e7292547774b11fe8c4738d8d1c53deab4
APurples <tylersanders0714@gmail.com>
2023-11-04 22:33:59 -0500

    HEAVY wip dialogue shit

---
+++ data/states/PlayState.hx
@@ -0,0 +1,12 @@
+
+/*
+function update(){
+    if (playCutscenes){
+        new FlxTimer().start(1.35, function(tmr:FlxTimer){
+			openSubState(new DialogueCutscene(dialogue, function() {
+				callback();
+			}));
+        });
+    }
+}
+*/
