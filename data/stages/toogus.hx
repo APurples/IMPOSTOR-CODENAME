7b4aa50d9d949933fcf78ae39350a5a9c1babdaf
APurples <tylersanders0714@gmail.com>
2023-11-06 20:32:26 -0600

    did some things to sussus toogus

--- data/stages/toogus.hx
+++ data/stages/toogus.hx
@@ -23,50 +23,20 @@ function stepHit(){
 				dad.playAnim("left", true);
 			case 124:
 				dad.playAnim("right", true);
+				if (PlayState.opponentMode || PlayState.coopMode) FlxTween.tween(camHUD, {alpha: 1}, .75);
 			case 128:
 				FlxTween.tween(camHUD, {alpha: 1}, .75);
 			case 896:
+				FlxTween.tween(camHUD, {alpha: 0}, .75);
 				saxguy.animation.play("POP OFF CYAN");
 				saxguy.alpha = 1;
 				dad.playAnim("notice", true);
+			case 1016:
+				if (PlayState.opponentMode || PlayState.coopMode) FlxTween.tween(camHUD, {alpha: 1}, .75);
+			case 1024:
+				FlxTween.tween(camHUD, {alpha: 1}, .75);
 		}
-	
-		if (curStep > 111 && curStep < 125){
-			if (curStep % 4 == 0){
-				camGame.zoom += 0.03;
-				camHUD.zoom += 0.045;
-			}
-		}
-		if (curStep > 239 && curStep < 249){
-			if (curStep % 4 == 0){
-				camGame.zoom += 0.03;
-				camHUD.zoom += 0.045;
-			}
-		}
-		if (curStep > 367 && curStep < 381){
-			if (curStep % 4 == 0){
-				camGame.zoom += 0.03;
-				camHUD.zoom += 0.045;
-			}
-		}
-		if (curStep > 381 && curStep < 384){
-			if (curStep % 1 == 0){
-				camGame.zoom += 0.03;
-				camHUD.zoom += 0.045;
-			}
-		}
-		if (curStep > 495 && curStep < 509){
-			if (curStep % 4 == 0){
-				camGame.zoom += 0.03;
-				camHUD.zoom += 0.045;
-			}
-		}
-		if (curStep > 509 && curStep < 512){
-			if (curStep % 1 == 0){
-				camGame.zoom += 0.03;
-				camHUD.zoom += 0.045;
-			}
-		}
+
 		if (curStep > 623 && curStep < 637){
 			if (curStep % 4 == 0){
 				camGame.zoom += 0.03;
